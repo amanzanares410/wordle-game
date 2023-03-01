852690a30956fa22e3c582888c1ded9fb301e678
Ashley Manzanares <fnv2vx@virginia.edu>
2023-02-28 21:56:20 -0500

    fixing bug in testInvalidInputGuessCount

--- src/test/java/edu/virginia/cs/hw2/GameTest.java
+++ src/test/java/edu/virginia/cs/hw2/GameTest.java
@@ -112,7 +112,9 @@ class GameTest {
     public void testInvalidInputGuessCount() {
         DefaultDictionaryFactory factory = new DefaultDictionaryFactory();
         testGame = new GameState("HELLO", factory.getDefaultGuessesDictionary(), 3, GameState.GameStatus.PLAYING);
-        testGame.submitGuess("He5LO");
+        assertThrows(IllegalWordException.class, () -> {
+            testGame.submitGuess("He5LO");
+        });
         assertEquals(3, testGame.getGuessCount());
     }
 
